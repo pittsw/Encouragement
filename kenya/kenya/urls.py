d821777a28e7da2f37af44122467fd19824c985f
David Swanson <swanson.david@gmail.com>
2012-04-30 21:38:23 -0700

    one more

--- kenya/kenya/urls.py
+++ kenya/kenya/urls.py
@@ -6,14 +6,14 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^patient/(?P<id_number>\d+)/', views.client),
-	url(r'^detail/(?P<id_number>\d+)/', views.detail),
-	url(r'^message/(?P<id_number>\d+)/', views.add_message),
-	url(r'^message/', views.add_message),
-	url(r'^demo/', views.list_clients),
-	url(r'^add/', views.add_client),
+    url(r'^patient/(?P<id_number>\d+)/', views.client),
+    url(r'^detail/(?P<id_number>\d+)/', views.detail),
+    url(r'^message/(?P<id_number>\d+)/', views.add_message),
+    url(r'^message/', views.add_message),
+    url(r'^demo/', views.list_clients),
+    url(r'^add/', views.add_client),
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^overview/', views.over),
+    url(r'^overview/', views.over),
     url(r'^smssync', views.smssync),
     url(r'^$', views.over),
 )
