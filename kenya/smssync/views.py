8ef66ca0e292fe6017bd461c1d06ae47ae9dde50
Will Pitts <infinity1998@gmail.com>
2012-06-07 02:47:14 +0000

    smssync no longer fails quietly

--- kenya/smssync/views.py
+++ kenya/smssync/views.py
@@ -1,6 +1,7 @@
 import sys
 
 from django.conf import settings
+from django.db import transaction
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import simplejson
@@ -11,39 +12,39 @@ from smssync.models import SMSSyncOutgoing
 @csrf_exempt
 def smssync(request):
     try:
-        secret = settings.SMSSYNC_SECRET
-        payload = {
-            "secret": secret,
-        }
+        with transaction.commit_on_success():
+            secret = settings.SMSSYNC_SECRET
+            payload = {
+                "secret": secret,
+            }
 
-        if request.method == 'POST':
-            sender = request.POST['from']
-            msg = request.POST['message']
-            print >> sys.stderr, "{sender}: {msg}".format(sender=sender, msg=msg)
-            sys.stderr.flush()
-            payload['success'] = "true" if incoming_message(sender, msg) else "false"
-        else:
-            print >> sys.stderr, "GET!?"
-            sys.stderr.flush()
+            if request.method == 'POST':
+                sender = request.POST['from']
+                msg = request.POST['message']
+                print >> sys.stderr, "{sender}: {msg}".format(sender=sender, msg=msg)
+                sys.stderr.flush()
+                payload['success'] = "true" if incoming_message(sender, msg) else "false"
+            else:
+                print >> sys.stderr, "GET!?"
+                sys.stderr.flush()
 
-        outgoing_messages = SMSSyncOutgoing.objects.all()
-        if len(outgoing_messages) > 0:
-            payload['task'] = "send"
-            messages = [{
-                "to": msg.target,
-                "message": msg.content,
-            } for msg in outgoing_messages]
+            outgoing_messages = SMSSyncOutgoing.objects.all()
+            if len(outgoing_messages) > 0:
+                payload['task'] = "send"
+                messages = [{
+                    "to": msg.target,
+                    "message": msg.content,
+                } for msg in outgoing_messages]
 
-            if ('HTTP_USER_AGENT' not in request.META
-                    or 'SMSSync' in request.META['HTTP_USER_AGENT']):
-                outgoing_messages.delete()
-            payload['messages'] = messages
+                if ('HTTP_USER_AGENT' not in request.META
+                        or 'SMSSync' in request.META['HTTP_USER_AGENT']):
+                    outgoing_messages.delete()
+                payload['messages'] = messages
 
-        reply = {
-            "payload": payload
-        }
-        return HttpResponse(simplejson.dumps(reply), mimetype="application/json")
+            reply = {
+                "payload": payload
+            }
+            return HttpResponse(simplejson.dumps(reply), mimetype="application/json")
     except Exception as e:
         print >> sys.stderr, "Exception: {e}".format(e=e)
         sys.stderr.flush()
-        return HttpResponse(str(e))
